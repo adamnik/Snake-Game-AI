efc5adc957f02c8630a70ff3dfe3df04c243bea2
adamnik <nika@carleton.edu>
2022-11-28 15:35:15 -0600

    updated graph

--- snakeGame.py
+++ snakeGame.py
@@ -14,7 +14,8 @@ from typing import Tuple, NamedTuple
 from collections import defaultdict, namedtuple
 import copy
 import argparse
-from graphics import *
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 # sets parameters for window size/display
 windowHeight = 600
@@ -34,8 +35,8 @@ Feature = NamedTuple('Feature', [('featureKey', Tuple), ('featureValue', int)])
 
 class Snake:
 
-	def __init__(self, apple, iterations, graph):
-	
+	def __init__(self, apple, iterations, fig, ax):
+
 		# green snake
 		self.color = (0,255,0)
 		self.x = 0
@@ -50,8 +51,10 @@ class Snake:
 		self.model = Model()
 		self.apple = apple
 		self.reward = 0
-		self.graph = graph
-		
+		self.results = {'x':[], 'y':[], 'avg':[]}
+		self.fig = fig
+		self.ax = ax
+
 	#reset the snake after each iteration of the game
 	def reset(self):
 		# sets initial location of snake randomly
@@ -59,17 +62,34 @@ class Snake:
 		self.y = random.randint(3, rowCount - 2) * cellSize
 		self.snakeBody = [{'x': self.x, 'y': self.y}]
 		self.reward = 0
-		
-	#print score and reset
+
+	def updateGraph(self, x, y):
+		if len(self.results['x']) < self.iterationLimit:
+			self.results['x'].append(x)
+			self.results['y'].append(y)
+			last10 = self.results['y'][-10:]
+			self.results['avg'].append(sum(last10)/len(last10))
+
+		def animate():
+			self.ax.clear()
+			self.ax.scatter(self.results['x'], self.results['y'])
+			self.ax.plot(self.results['x'], self.results['avg'], label='Previous 10 Iteration Average')
+
+		animate()
+		plt.legend(loc='upper left')
+		plt.xlabel('Iteration')
+		plt.ylabel('Score')
+		plt.draw()
+
+	#reset
 	def endGame(self):
-		printScore(self)
-		self.graph.plotPoint(self.iteration, self.getScore(), self.iterationLimit)
+		self.updateGraph(self.iteration, self.getScore())
 		self.reset()
 		self.iteration += 1
-		
+
 	# draws the initial snake head --> the first segment of the snake
 	def drawSnake(self, win):
-	
+
 		# draws cube for each segment stored in snakeBody
 		for i in range(len(self.snakeBody)):
 			cube = pygame.Rect(self.snakeBody[i]['x'], self.snakeBody[i]['y'], cellSize, cellSize)
@@ -82,21 +102,21 @@ class Snake:
 				eye2 = pygame.Rect(self.snakeBody[i]['x'] + (cellSize/2) + 5, self.snakeBody[i]['y'] + eyeHeight, eyeWidth, eyeHeight)
 				pygame.draw.ellipse(win, (0,0,0), eye1)
 				pygame.draw.ellipse(win, (0,0,0), eye2)
-		
+
 		pygame.display.update()
 
 	def move(self, win, FPS):
-	
+
 		#randomly assigns a direction to the snake at the beginning of each game
 		self.direction = random.choice(directions)
 		while True:
 			for event in pygame.event.get():
-				if event.type == QUIT:	
+				if event.type == QUIT:
 					pygame.quit()
 					exit()
 			# clock affects how often the screen is updated, a higher FPS will make the snake move faster
 			clock.tick(FPS)
-			
+
 			#define state for Q function
 			curSnakeHead, curSnakeBody, curApple = copy.deepcopy(self.getHead()), copy.deepcopy(self.snakeBody), copy.deepcopy(self.apple)
 			state = State(curSnakeHead, curSnakeBody, curApple)
@@ -104,31 +124,31 @@ class Snake:
 			attachHead = moveHead(self.getHead(), self.direction)
 			apple = False
 			collision = False
-		
 
-			# Add new section for head		
+
+			# Add new section for head
 			self.snakeBody.insert(0, attachHead)
 			#new state for incorporate feedback
 			newState = State(self.getHead(), self.snakeBody, self.apple)
-		
+
 			# checks with collision with apple
 			if self.snakeBody[0]['x'] == self.apple.getX() and self.snakeBody[0]['y'] == self.apple.getY():
-				#randomizes the location of the next apple	
+				#randomizes the location of the next apple
 				self.apple.setRandomX()
 				self.apple.setRandomY()
 				self.apple.drawApple(win)
 				self.drawSnake(win)
-				pygame.display.update()		
+				pygame.display.update()
 				apple = True
 
 			else:
 				# removes last element, if no apple eaten
-				self.snakeBody.pop() 
+				self.snakeBody.pop()
 				drawWin(win)
 				self.apple.drawApple(win)
 				self.drawSnake(win)
 				pygame.display.update()
-						
+
 			# reset after snake hits border
 			if self.snakeBody[0]['x'] <= -1 or self.snakeBody[0]['x'] >= windowWidth or self.snakeBody[0]['y'] <= -1 or self.snakeBody[0]['y'] >= windowHeight:
 				collision = True
@@ -139,7 +159,7 @@ class Snake:
 				if self.iteration >= self.iterationLimit:
 					return
 				self.move(win, FPS)
-		
+
 			else:
 				# reset after snake hits itself
 				for seg in self.snakeBody[1:]:
@@ -153,26 +173,26 @@ class Snake:
 						if self.iteration >= self.iterationLimit:
 							return
 						self.move(win, FPS)
-						
+
 			reward = self.model.getReward(state, self.direction, apple, collision)
 			self.model.incorporateFeedback(state, self.direction, reward, newState, possibleDirections(self.direction))
 			if self.iteration >= self.iterationLimit:
 				return
-						
+
 
     #picks action based of E-greedy algorithm
 	def changeDirection(self, state):
 		newDirection = self.model.getAction(state, possibleDirections(self.direction))
 		self.direction = newDirection
-		
+
 	# score is the number of added segments
 	def getScore(self):
 		self.score = len(self.snakeBody) - 1
 		return self.score
-		
+
 	def getHead(self):
 		return self.snakeBody[0]
-		
+
 #snake can't go in opposite of current direction
 def getOppositeDirection(direction):
 	if direction == UP:
@@ -197,17 +217,14 @@ def moveHead(head, direction):
 
 	elif direction == LEFT:
 		return {'x': head['x'] - cellSize, 'y': head['y']}
-		
-def printScore(snake):
-	print("Iteration:{}, Score:{}".format(snake.iteration, snake.getScore()))
-	
+
 def possibleDirections(curDirection):
 	possibleDirections = []
 	for dir in directions:
 		if dir != getOppositeDirection(curDirection):
 			possibleDirections.append(dir)
 	return possibleDirections
-		
+
 class Apple:
 
 	def __init__(self):
@@ -217,27 +234,27 @@ class Apple:
 		self.y = 0
 
 	def drawApple(self, win):
-	
+
 		# apple is red cube
 		self.apple = pygame.Rect(self.x, self.y, cellSize, cellSize)
 		pygame.draw.rect(win, self.color, self.apple)
-		
+
 		pygame.display.update()
-		
+
 	# randomly selects x location for apple
 	def setRandomX(self):
 		self.x = random.randint(2, columnCount - 1) * cellSize
-		
+
 	def getX(self):
 		return self.x
-		
+
 	# randomly selects y location for apple
 	def setRandomY(self):
 		self.y = random.randint(2, rowCount - 1) * cellSize
-		
+
 	def getY(self):
 		return self.y
-		
+
 class State:
 	def __init__(self, snakeHead, snakeBody, apple):
 		self.snakeHead = snakeHead
@@ -250,14 +267,14 @@ class Model:
 		self.weights = defaultdict(float)
 		self.features = []
 		self.iterations = 0
-		
+
 	#Q function score
 	def getQ(self, state, action):
 		score = 0
 		for feature, value in self.getFeatures(state, action):
 			score += self.weights[feature] * value
 		return score
-		
+
 	#gets action based off E-greedy algorithm
 	def getAction(self, state, actions):
 		self.iterations += 1
@@ -267,10 +284,10 @@ class Model:
 		else:
 			action = random.choice(actions)
 			return action
-		
+
 	def getAlpha(self):
 		return 1.0 / math.sqrt(self.iterations)
-		
+
 	#dynamic exploration probability
 	def getExploration(self):
 		rate = 0.5 - (0.01 * self.iterations)
@@ -278,29 +295,29 @@ class Model:
 			return rate
 		else:
 			return 0.01
-		
+
 	'''
 	Features of given state (formed into a tuple):
-	
+
 		1) Apple above snake
 		2) Apple below snake
 		3) Apple to the left of snake
 		4) Apple to the right of snake
-		
+
 		5) Danger above snake
 		6) Danger below snake
 		7) Danger to the left of snake
 		8) Danger to the right of snake
-		
+
 		9) Direction = Up
 		10) Direction = Down
 		11) Direction = Left
 		12) Direction = Right
-		
+
 	'''
 	def getFeatures(self, state, action):
 		features = []
-		
+
 		appleVert = state.apple.getY() - state.snakeHead['y']
 		if appleVert < 0:
 			features.append(1)
@@ -310,7 +327,7 @@ class Model:
 			features.append(1)
 		else:
 			features.append(0)
-			
+
 		appleHor = state.apple.getX() - state.snakeHead['x']
 		if appleHor < 0:
 			features.append(1)
@@ -320,7 +337,7 @@ class Model:
 			features.append(1)
 		else:
 			features.append(0)
-	
+
 		for key in ['y', 'x']:
 			for num in [-cellSize, cellSize]:
 				point = copy.deepcopy(state.snakeHead)
@@ -329,18 +346,18 @@ class Model:
 					features.append(1)
 				else:
 					features.append(0)
-					
+
 		for dir in directions:
 			if dir == action:
 				features.append(1)
 			else:
 				features.append(0)
-				
+
 		features = tuple(features)
-		
+
 		return [Feature(featureKey=features, featureValue=1)]
-	
-		
+
+
 	def getReward(self, state, action, apple, collision):
 		'''
 		1) +30 for getting apple
@@ -349,122 +366,74 @@ class Model:
 		'''
 		reward = 0
 		newHeadLocation = moveHead(state.snakeHead, action)
-		
+
 		#reward of getting apple
 		if apple:
 			reward += 30
-		
+
 		#dying
 		if collision:
 			reward -= 50
-		
+
 		#reward getting closer to apple
 		Xdist = (abs(state.apple.getX() - state.snakeHead['x']) - abs(state.apple.getX() - newHeadLocation['x'])) / cellSize
 		Ydist = (abs(state.apple.getY() - state.snakeHead['y']) - abs(state.apple.getY() - newHeadLocation['y'])) / cellSize
-		
+
 		reward += Xdist + Ydist
-					
+
 		return reward
-		
+
 	def incorporateFeedback(self, state, action, reward, newState, actions):
 		features = self.getFeatures(state, action)
 		maxQ = max((self.getQ(newState, action), action) for action in actions)[0]
 		for featureKey, featureValue in features:
 			self.weights[featureKey] += self.getAlpha() * ((reward + (self.discount * maxQ)) - self.getQ(state, action)) * featureValue
 
-#for plotting results
-class Graph:
-	
-	def __init__(self, windowHeight, windowWidth, iterations):
-		self.windowHeight = windowHeight
-		self.windowWidth = windowWidth
-		self.win = GraphWin('Results', self.windowHeight, self.windowWidth)
-		self.drawAxis(iterations)
-		
-	def drawAxis(self, iterations):
-		self.win.setBackground(color_rgb(255,255,255))
-		self.win.setCoords(0,0,windowWidth,windowHeight)
-		Xaxis = Line(Point(cellSize, cellSize), Point((columnCount - 1)* cellSize, cellSize))
-		Xaxis.draw(self.win)
-		Yaxis = Line(Point(cellSize, cellSize), Point(cellSize, (rowCount - 1) * cellSize))
-		Yaxis.draw(self.win)
-
-		Xlabel = Text(Point(300, 25), "Number of Iterations")
-		Xlabel.draw(self.win)
-		Ylabel = Text(Point(25, 550), "Score")
-		Ylabel.draw(self.win)
-		
-		#x-axis
-		for i in range(9):
-			x, y = cellSize*(i+1), cellSize
-			line = Line(Point(x,y), Point(x,y-6))
-			line.draw(self.win)
-			number = int(iterations /8 * i)
-			increment = Text(Point(x, y-10), "{}".format(number))
-			increment.setSize(6)
-			increment.draw(self.win)
-			
-		#y-axis
-		for i in range(9):
-			x,y = cellSize, cellSize*(i+1)
-			line = Line(Point(x,y), Point(x-5,y))
-			line.draw(self.win)	
-			number = 5*i
-			increment = Text(Point(x-9, y), "{}".format(number))
-			increment.setSize(6)
-			increment.draw(self.win)	
-
-	def plotPoint(self, x, y, iterations):
-		x, y = x  * ((columnCount - 2) * cellSize / iterations)+ cellSize, y * ((columnCount - 2) * cellSize / 40) + cellSize
-		point = Circle(Point(x, y), 2)
-		point.setFill(color_rgb(0,0,0))
-		point.draw(self.win)
-	
 # draws window for snake
 def drawWin(win):
 
 	# black background
 	win.fill((0,0,0))
-	
+
 	x = 0
 	y = 0
-	
+
 	# draws the grid using lines across the window
 	for line in range(windowWidth // cellSize):
 		x = x + cellSize
 		pygame.draw.line(win, (255,255,255), (x,0), (x, windowWidth))
 		y = y + cellSize
 		pygame.draw.line(win, (255,255,255), (0,y), (windowHeight, y))
-		
+
 	pygame.display.update()
-	
+
 def main():
-	
+
 	#user specified FPS and number of game iterations
 	parser = argparse.ArgumentParser(description = "Snake Game with Reinforcement Learning")
-	parser.add_argument("-FPS", 
-						help = "Specifies the speed of game. Example: -FPS=15", 
-						required = False, 
-						default = 25, 
+	parser.add_argument("-FPS",
+						help = "Specifies the speed of game. Example: -FPS=15",
+						required = False,
+						default = 25,
 						type = int)
-	parser.add_argument("-iterations", 
-						help = "Specifies the number of iterations of the game. Example: -iterations=150", 
-						required = False, 
-						default = 200, 
+	parser.add_argument("-iterations",
+						help = "Specifies the number of iterations of the game. Example: -iterations=150",
+						required = False,
+						default = 200,
 						type = int)
-	args = parser.parse_args()	
-		
+	args = parser.parse_args()
+
 	#initializes the game
 	pygame.init()
-	
+
 	# defines the windows
 	win = pygame.display.set_mode((windowHeight, windowWidth))
 
 	# draws game window
 	drawWin(win)
-	
-	#plotting results
-	graph = Graph(windowHeight, windowWidth, args.iterations)
+
+	plt.ion()
+	fig, ax = plt.subplots()
 
 	# initializes apple
 	apple = Apple()
@@ -473,19 +442,19 @@ def main():
 	apple.drawApple(win)
 
 	# initializes snake
-	snake = Snake(apple, args.iterations, graph)
+	snake = Snake(apple, args.iterations, fig, ax)
 	snake.drawSnake(win)
 
 	# moves snake until there is collision with self/border
 	snake.move(win, args.FPS)
-	
+
 	#keep windows open until program is exited
 	print("Click red 'x' on pygame window to terminate program.")
 	while True:
 		for event in pygame.event.get():
-			if event.type == QUIT:	
+			if event.type == QUIT:
 				pygame.quit()
 				exit()
-		
+
 if __name__ == '__main__':
 	main()
